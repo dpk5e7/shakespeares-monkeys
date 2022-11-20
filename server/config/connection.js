fc7648289b8d139a2194037be29be626dadb2375
Dan Kelly <dpk5e7@yahoo.com>
2022-11-19 23:06:35 -0700

    removing bcrypt in favor of crypto-js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/shakespeares-monkeys",
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/team-handbook",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,

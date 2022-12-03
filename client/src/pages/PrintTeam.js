344e7f1aaebe711b308a0c14d68a56dc7e49a1f0
Rod Bennett <rodbennett@Rods-Air-2.lan>
2022-12-03 14:10:42 -0700

    team page dynamically rendering, styled

--- client/src/pages/PrintTeam.js
+++ client/src/pages/PrintTeam.js
@@ -2,41 +2,40 @@ import { useQuery } from "@apollo/client";
 import { GET_MY_TEAM } from "../utils/queries";
 import TeamMemberTable from "../components/TeamMemberTable";
 import { Table } from "semantic-ui-react";
-// import UpcomingDates from "../components/UpcomingDates";
-
+import { Icon } from "semantic-ui-react";
 
 const PrintTeam = () => {
+
   const { loading, error, data } = useQuery(GET_MY_TEAM);
   const teamData = data?.team || [];
-
-
+  if (loading) {
+    return <div>Loading...</div>;
+  }
   return (
     <>
-    <Table>
-      {teamData.map((teamMember) => (
-        <TeamMemberTable 
-        id={teamMember._id}
-        name={teamMember.name}
-        email={teamMember.contactInfo.email}
-        phoneNumber={teamMember.contactInfo.phoneNumber}
-        emergencyPOCName={teamMember.emergencyPOC.name}
-        emergencyPOCRelationship={teamMember.emergencyPOC.relationship}
-        emergencyPOCPhoneNumber={teamMember.emergencyPOC.phoneNumber}
-        skills={teamMember.skills}
-        responsibilities={teamMember.responsibilities}
-        experience={teamMember.experience}
-        training={teamMember.training}
-        mailingAddress={teamMember.contactInfo.mailingAddress}
-        familySituation={teamMember.familySituation}
-        importantDates={teamMember.importantDates.importantDate}
-        importantDatesDescription={teamMember.importantDates.description}
-        />
-
-      ))}
-      
-    </Table>
+        <Table>
+          {teamData.map((teamMember) => (
+            <TeamMemberTable
+              id={teamMember._id}
+              name={teamMember.name}
+              email={teamMember.contactInfo.email}
+              phoneNumber={teamMember.contactInfo.phoneNumber}
+              emergencyPOCName={teamMember.emergencyPOC.name}
+              emergencyPOCRelationship={teamMember.emergencyPOC.relationship}
+              emergencyPOCPhoneNumber={teamMember.emergencyPOC.phoneNumber}
+              skills={teamMember.skills}
+              responsibilities={teamMember.responsibilities}
+              experience={teamMember.experience}
+              training={teamMember.training}
+              mailingAddress={teamMember.contactInfo.mailingAddress}
+              familySituation={teamMember.familySituation}
+              importantDates={teamMember.importantDates.importantDate}
+              importantDatesDescription={teamMember.importantDates.description}
+            />
+          ))}
+        </Table>
     </>
-  )
+  );
 }
 
 export default PrintTeam
